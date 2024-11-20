7f70edf21e9aa123449b7effb5c2bb4f24a18e70
yhbetty <yhbettychen@gmail.com>
2024-11-20 14:56:25 +0800

    1120-1

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   // 建立一個開發中的路徑
   // process.env.NODE_ENV =>　取得node環境中的變數
   // 如果目前路徑等於產品路徑，就執行倉庫路徑
-  base:process.env.NODE_ENV === 'production' ?'/myReact/' :'/',
+  base:process.env.NODE_ENV === 'production' ?'/Todolist/' :'/',
   plugins: [react()],
 })
